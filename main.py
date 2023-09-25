221118408ffcb910ca4b402887f9cb8ad7a4f09b
defenetlynotjhonny <krstinikola@gmail.com>
2023-09-25 03:18:48 +0200

    Added New Data

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("test")
-print("Hello")
-print("Hi from other PC")
+# libraries instabot ; openpyxl; pillow
+
+from instabot import Bot
+
+from PIL import Image, ImageDraw, ImageFont
+
+font = ImageFont.truetype("Tests/fonts/NotoSans-Regular.ttf", 48)
+im = Image.new("RGB", (200, 200), "white")
+d = ImageDraw.Draw(im)
+d.line(((0, 100), (200, 100)), "gray")
+d.line(((100, 0), (100, 200)), "gray")
+d.text((100, 100), "Quick", fill="black", anchor="ms", font=font)
